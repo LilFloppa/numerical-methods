1ebcbe99c5c3c231196107a6f696edca6162c83d
Nikita <nikita.nstu@gmail.com>
2019-10-31 15:35:26 +0700

    CGM + LOS without factorization

--- CGM/Source/Matrix/Matrix.cpp
+++ CGM/Source/Matrix/Matrix.cpp
@@ -1,42 +1,42 @@
 #include "Matrix.h"
 
-void ReadMatrix(Matrix& m, int& maxiter, double& eps)
+void ReadMatrix(Matrix& A, int& Aaxiter, double& eps)
 {
 	std::ifstream in("input/kuslau.txt");
-	in >> m.N >> maxiter >> eps;
+	in >> A.N >> Aaxiter >> eps;
 	in.close();
 
-	m.IA = new int[m.N + 1];
-	m.DI = new double[m.N];
+	A.IA = new int[A.N + 1];
+	A.DI = new double[A.N];
 
 	in.open("input/ig.txt");
-	for (int i = 0; i < m.N + 1; i++)
-		in >> m.IA[i];
+	for (int i = 0; i < A.N + 1; i++)
+		in >> A.IA[i];
 	in.close();
 
-	int size = m.IA[m.N];
-	m.JA = new int[size];
-	m.AL = new double[size];
-	m.AU = new double[size];
+	int size = A.IA[A.N];
+	A.JA = new int[size];
+	A.AL = new double[size];
+	A.AU = new double[size];
 
 	in.open("input/jg.txt");
-	for (int i = 0; i < m.IA[m.N]; i++)
-		in >> m.JA[i];
+	for (int i = 0; i < A.IA[A.N]; i++)
+		in >> A.JA[i];
 	in.close();
 
 	in.open("input/ggl.txt");
-	for (int i = 0; i < m.IA[m.N]; i++)
-		in >> m.AL[i];
+	for (int i = 0; i < A.IA[A.N]; i++)
+		in >> A.AL[i];
 	in.close();
 
 	in.open("input/ggu.txt");
-	for (int i = 0; i < m.IA[m.N]; i++)
-		in >> m.AU[i];
+	for (int i = 0; i < A.IA[A.N]; i++)
+		in >> A.AU[i];
 	in.close();
 
 	in.open("input/di.txt");
-	for (int i = 0; i < m.N; i++)
-		in >> m.DI[i];
+	for (int i = 0; i < A.N; i++)
+		in >> A.DI[i];
 	in.close();
 }
 
@@ -62,4 +62,13 @@ void Multiply(Matrix& A, double* vec, double* res)
 			res[col] += A.AU[j] * vec[i];
 		}
 	}
+}
+
+double DotProduct(int N, double* a, double* b)
+{
+	double sum = 0;
+	for (int i = 0; i < N; i++)
+		sum += a[i] * b[i];
+
+	return sum;
 }
--- CGM/Source/Matrix/Matrix.h
+++ CGM/Source/Matrix/Matrix.h
@@ -11,6 +11,7 @@ struct Matrix
 	int* JA;
 };
 
-void ReadMatrix(Matrix& m, int& maxiter, double& eps);
+void ReadMatrix(Matrix& A, int& Aaxiter, double& eps);
 void ReadB(int N, double* b);
 void Multiply(Matrix& A, double* vec, double* res);
+double DotProduct(int N, double* a, double* b);
