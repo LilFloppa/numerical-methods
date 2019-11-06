272ff33d8983916533b15803242cc7722bd2bbe8
Nikita <nikita.nstu@gmail.com>
2019-11-06 22:56:42 +0700

    refactoring

--- CGM/Source/Matrix/Matrix.h
+++ CGM/Source/Matrix/Matrix.h
@@ -4,11 +4,21 @@
 struct Matrix
 {
 	int N;
-	double* DI;
-	double* AL;
-	double* AU;
-	int* IA;
-	int* JA;
+	double* DI = nullptr;
+	double* AL = nullptr;
+	double* AU = nullptr;
+	int* IA = nullptr;
+	int* JA = nullptr;
+};
+
+struct AuxVectors
+{
+	double* Ax = nullptr;
+	double* r = nullptr;
+	double* z = nullptr;
+	double* p = nullptr;
+	double* LU = nullptr;
+	double* temp = nullptr;
 };
 
 void ReadMatrix(Matrix& A, int& Aaxiter, double& eps, int& choice);
