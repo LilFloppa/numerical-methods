8ce65eb61bd6c16eca44fb08f347d7aa8562df02
Никита Скрынник <nikita.nstu@gmail.com>
2021-10-02 22:01:30 +0700

    fixes

--- Nonlinear Inverse Problem/NonlinearInverseProblem/Program.cs
+++ Nonlinear Inverse Problem/NonlinearInverseProblem/Program.cs
@@ -40,7 +40,6 @@ namespace NonlinearInverseProblem
                 writer.WriteLine($"{ trueI }; { TrueV[0] }; { TrueV[1] }; 0");
                 writer.WriteLine($"{ I0 }; { V0[0] }; { V0[1] }; { J }");
 
-                writer.WriteLine(I0);
                 while (Math.Abs(I0 - trueI) > iEps)
                 {
                     double dI = Problem.InverseProblemLinear(info, V0, TrueV, eps, 0.05 * I0);
@@ -149,6 +148,12 @@ namespace NonlinearInverseProblem
 
         static void Main(string[] args)
         {
+            System.Globalization.CultureInfo culture = System.Threading.Thread.CurrentThread.CurrentCulture.Clone() as System.Globalization.CultureInfo ?? throw new InvalidCastException();
+            culture.NumberFormat = System.Globalization.CultureInfo.InvariantCulture.NumberFormat;
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+            System.Globalization.CultureInfo.DefaultThreadCurrentCulture = culture;
+            System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = culture;
+
             LinearProblem();
         }
     }
