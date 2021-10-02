ca3a610f9ef2b49f4886a0e67846cdda9ad33e36
Никита Скрынник <nikita.nstu@gmail.com>
2021-10-02 21:38:45 +0700

    update linear problem

--- Nonlinear Inverse Problem/NonlinearInverseProblem/Program.cs
+++ Nonlinear Inverse Problem/NonlinearInverseProblem/Program.cs
@@ -9,8 +9,11 @@ namespace NonlinearInverseProblem
     {
         static void LinearProblem()
         {
+            Random random = new Random();
+            double trueI = 1.66;
+            double iEps = 0.1;
             double eps = 1.0e-15;
-            Source s = new Source(new Vector2(0, 0), new Vector2(100, 0), 1.6);
+            Source s = new Source(new Vector2(0, 0), new Vector2(100, 0), trueI);
             Receiver r1 = new Receiver(new Vector2(800, 0));
             Receiver r2 = new Receiver(new Vector2(900, 0));
 
@@ -25,17 +28,51 @@ namespace NonlinearInverseProblem
             };
 
             double[] TrueV = Problem.DirectProblem(info, eps);
+            TrueV[0] += TrueV[0] * (0.01 + 0.9 * random.NextDouble());
 
             double I0 = 5.0;
-            info.Source.I = 5.0;
+            info.Source.I = I0;
             double[] V0 = Problem.DirectProblem(info, eps);
 
-            double dI = Problem.InverseProblemLinear(info, V0, TrueV, eps, 0.05 * I0);
+            double J = Problem.Functional(V0, TrueV);
+            using (var writer = new StreamWriter(File.OpenWrite("I.txt")))
+            {
+                writer.WriteLine(I0);
+                while (Math.Abs(I0 - trueI) > iEps)
+                {
+                    double dI = Problem.InverseProblemLinear(info, V0, TrueV, eps, 0.05 * I0);
 
-            double I1 = I0 + dI;
-            Console.WriteLine(I1);
+                    double b = 1.0;
+                    double Ii = I0 + b * dI;
+
+                    info.Source.I = Ii;
+                    V0 = Problem.DirectProblem(info, eps);
+
+                    Console.WriteLine("Вычисление нового I");
+                    int k = 0;
+                    while (b > 1.0e-5 && Problem.Functional(V0, TrueV) >= J)
+                    {
+                        Console.WriteLine($"Итерация: { k++ }, Ii: { Ii } ");
+                        b /= 2;
+                        Ii = I0 + b * dI;
+                        info.Source.I = dI;
+                        V0 = Problem.DirectProblem(info, eps);
+                    }
+
+                    if (b <= 1.0e-5)
+                    {
+                        Console.WriteLine($"b is too small. Last I: { I0 } ");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"New I: { Ii }");
+                        I0 = Ii;
+                        writer.WriteLine(I0);
+                    }
+                }
+            }
         }
-
         static void NonlinearProblem()
         {
             double eps = 1.0e-15;
@@ -56,19 +93,20 @@ namespace NonlinearInverseProblem
                 SolverType = SlaeSolver.SolverTypes.LOSLU
             };
 
+
+            double[] TrueV = Problem.DirectProblem(info, eps);
+
+            double sigma0 = 4.0;
+
+
+            Console.WriteLine($"new sigma { sigma0 }");
+            info.sigma1 = sigma0;
+            double[] V0 = Problem.DirectProblem(info, eps);
+
+            double J = Problem.Functional(V0, TrueV);
             using (var writer = new StreamWriter(File.OpenWrite("sigma.txt")))
             {
-                double[] TrueV = Problem.DirectProblem(info, eps);
-
-                double sigma0 = 4.0;
-
                 writer.WriteLine(sigma0);
-                Console.WriteLine($"new sigma { sigma0 }");
-                info.sigma1 = sigma0;
-                double[] V0 = Problem.DirectProblem(info, eps);
-
-                double J = Problem.Functional(V0, TrueV);
-
                 while (Math.Abs(sigma0 - trueSigma) > sigmaEps)
                 {
                     double ds = Problem.InverseProblem(info, V0, TrueV, eps, 0.05 * sigma0);
@@ -107,7 +145,7 @@ namespace NonlinearInverseProblem
 
         static void Main(string[] args)
         {
-            NonlinearProblem();
+            LinearProblem();
         }
     }
 }
