aa96914e813ee2b6e60bf651f774b170ccb2b8eb
LilFloppa <skrynnik.2017@stud.nstu.ru>
2022-06-06 12:01:39 +0700

    fix portrait builder error

--- OrderHarmonization/OrderHarmonization/PortraitBuilder.cs
+++ OrderHarmonization/OrderHarmonization/PortraitBuilder.cs
@@ -5,66 +5,63 @@ using System.Collections.Generic;
 namespace OrderHarmonization
 {
     public class PortraitBuilder
-	{
-		private ProblemInfo info;
-		private int nodeCount;
-		private SortedSet<int>[] connections;
+    {
+        private ProblemInfo info;
+        private int nodeCount;
+        private SortedSet<int>[] connections;
 
-		public PortraitBuilder(ProblemInfo info)
-		{
-			this.info = info;
-			nodeCount = info.Mesh.NodeCount;
-			connections = new SortedSet<int>[nodeCount];
-			for (int i = 0; i < nodeCount; i++)
-				connections[i] = new SortedSet<int>();
+        public PortraitBuilder(ProblemInfo info)
+        {
+            this.info = info;
+            nodeCount = info.Mesh.NodeCount;
+            connections = new SortedSet<int>[nodeCount];
+            for (int i = 0; i < nodeCount; i++)
+                connections[i] = new SortedSet<int>();
 
-			BuildConnections(info.Mesh);
-		}
+            BuildConnections(info.Mesh);
+        }
 
-		public Portrait Build()
-		{
-			Portrait p = new Portrait();
-			p.IA = new int[nodeCount + 1];
-			p.IA[0] = p.IA[1] = 0;
+        public Portrait Build()
+        {
+            Portrait p = new Portrait();
+            p.IA = new int[nodeCount + 1];
+            p.IA[0] = p.IA[1] = 0;
 
-			for (int i = 2; i <= nodeCount; i++)
-			{
-				int col = p.IA[i - 1];
-				p.IA[i] = col + connections[i - 1].Count;
-			}
+            for (int i = 2; i <= nodeCount; i++)
+            {
+                int col = p.IA[i - 1];
+                p.IA[i] = col + connections[i - 1].Count;
+            }
 
-			p.JA = new int[p.IA[nodeCount]];
+            p.JA = new int[p.IA[nodeCount]];
 
-			for (int i = 1, k = 0; i < nodeCount; i++)
-				foreach (int j in connections[i])
-				{
-					p.JA[k] = j;
-					k++;
-				}
+            for (int i = 1, k = 0; i < nodeCount; i++)
+                foreach (int j in connections[i])
+                {
+                    p.JA[k] = j;
+                    k++;
+                }
 
-			return p;
-		}
+            return p;
+        }
 
-		private void BuildConnections(Mesh mesh)
-		{
-			foreach (FiniteElement e in mesh.Elements)
-			{
-				for (int i = 1; i < info.Basis.Size; i++)
-				{
-					int a = e.Vertices[i];
-					if (a != -1)
-					{
-						for (int j = 0; j < i; j++)
-						{
-							int b = e.Vertices[j];
-							if (a < b) (a, b) = (b, a);
+        private void BuildConnections(Mesh mesh)
+        {
+            foreach (FiniteElement e in mesh.Elements)
+            {
+                for (int i = 1; i < info.Basis.Size; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        int a = e.Vertices[i];
+                        int b = e.Vertices[j];
+                        if (a < b) (a, b) = (b, a);
 
-							if (b != -1)
-								connections[a].Add(b);
-						}
-					}
-				}
-			}
-		}
-	};
+                        if (b != -1 && a != -1)
+                            connections[a].Add(b);
+                    }
+                }
+            }
+        }
+    };
 }
